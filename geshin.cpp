d7812df485b913b9264ac838ad4992665243c64d
Kyfaiyya <151041878+Kyfaiyya@users.noreply.github.com>
2024-06-10 00:11:32 +0800

    Update geshin.cpp

--- geshin.cpp
+++ geshin.cpp
@@ -8,7 +8,9 @@
 
 
 
-/* 1. Muhammad Faqih Husain   5027231023
+/* 
+               IT 16
+   1. Muhammad Faqih Husain   5027231023
    2. Muhammad Hildan Adiwena	50272310
    3. Dzaky Faiq Fayyadhi     5027231047
 */
